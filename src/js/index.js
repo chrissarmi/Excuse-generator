4b2c4646cda93ea644f13b5917cef6b05dd7a911
chrissarmi <chrissarmi@gmail.com>
2019-12-19 01:08:11 +0000

    this is my first js project

--- src/js/index.js
+++ src/js/index.js
@@ -8,4 +8,18 @@ import "../style/index.scss";
 
 window.onload = function() {
   console.log("Hello Rigo from the console!");
+  let who = ["My Neighbor", "My grandma", "My Dog"];
+  let what = ["Ate my food", "Stole my pc", "Woke me up"];
+  let when = ["After my class", "Before I left", "During my class"];
+
+  let theExcuse =
+    who[myfunction(who.length)] +
+    what[myfunction(what.length)] +
+    when[myfunction(when.length)];
+
+  document.querySelector("#excuse").innerHTML = theExcuse;
 };
+
+function myfunction(length) {
+  return Math.floor(Math.random() * (length - 1) + 0);
+}
